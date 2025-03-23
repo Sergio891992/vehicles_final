601b17cdcd8ac225410e670e52c7f21413f733a7
Sergio <sergio891992@gmail.com>
2025-03-23 16:33:47 -0600

    Actualización de archivo README y corrección de código.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # vehicles_final
+Esta aplicación permite generar una de dos gráficas útiles para la generación de gráficas que nos permiten el análisis de información proyectada para la toma de decisiones en cuanto al precio de autos y su kilometraje, así como los anuncios de los autos. 
--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pandas as pd
-import plotly as plt
+import plotly.express as px
+
 car_data = pd.read_csv(r'C:\Users\Sergio\Desktop\Tripleten\vehicles_final\vehicles_us.csv')
 
 hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de auncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir una gráfica de dispersión')
+
+if disp_button:
+    st.write('Creación de gráfico de dispersión con relación de precio de autos y kilometraje')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
